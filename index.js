161db74b85bac8807a477fb7b0ded4de96fc6252
Yue Su <yuesu@Mac-Pro.local>
2020-06-12 14:26:30 -0300

    finished stretch 2

--- index.js
+++ index.js
@@ -367,11 +367,19 @@ function getHTML(/* Code here */){
 
 /* STRETCH 2: Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
 
-function randomize(/* Code here */){
-
-    /* Code here */
-
+function randomize(arr){
+    const copyArtists = [...artists];
+    const randomArr = []; 
+    const finalArr =[];
+    for(let i = arr.length; i > 0; i--) {
+      let arrLength = arr.length;
+      let index = Math.floor(Math.random() * i);
+      randomArr.push(copyArtists.splice(index, 1));  
+    }
+    return randomArr.flat();
   }
 
+  console.log(randomize(artists));
+  console.log(artists);
 
  /* STRETCH 3: Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
